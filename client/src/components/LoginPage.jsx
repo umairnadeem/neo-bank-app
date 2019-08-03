fd9ddb1c5e97431031ad345a8c26c100eff31290
umairnadeem123 <nadeem.umair123@gmail.com>
2019-08-02 21:31:00 -0700

    Create MFA authentication React component

--- client/src/components/LoginPage.jsx
+++ client/src/components/LoginPage.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { userActions } from '../_actions';
+import Authenticate from './Authenticate';
 
 const actions = {
   login: userActions.login,
   logout: userActions.logout,
 };
+
+const mapStateToProps = state => ({
+  authentication: state.authentication,
+});
 
 class LoginPage extends React.Component {
   constructor(props) {
@@ -38,26 +43,34 @@ class LoginPage extends React.Component {
   }
 
   render() {
-    // const { result } = this.props;
+    const { authentication } = this.props;
     const { username, password } = this.state;
+
     return (
-      <form name="form" onSubmit={this.handleSubmit}>
-        <h1>Login</h1>
-        <label htmlFor="username">
-          Username:
-          <input name="username" type="text" value={username} onChange={this.handleChange} />
-        </label>
-        <label htmlFor="password">
-          Password:
-          <input name="password" type="text" value={password} onChange={this.handleChange} />
-        </label>
-        <button type="submit">Submit</button>
-      </form>
+      <div>
+        <form name="form" onSubmit={this.handleSubmit}>
+          <h1>Login</h1>
+          <label htmlFor="username">
+            Username:
+            <input name="username" type="text" value={username} onChange={this.handleChange} />
+          </label>
+          <label htmlFor="password">
+            Password:
+            <input name="password" type="text" value={password} onChange={this.handleChange} />
+          </label>
+          <button type="submit">Submit</button>
+        </form>
+        { authentication.requiresAuth
+          ? <Authenticate />
+          : null}
+      </div>
     );
   }
 }
+
 LoginPage.propTypes = {
   login: PropTypes.func.isRequired,
+  authentication: PropTypes.objectOf(PropTypes.any).isRequired,
 };
 
-export default connect(null, actions)(LoginPage);
+export default connect(mapStateToProps, actions)(LoginPage);
