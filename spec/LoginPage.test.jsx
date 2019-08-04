5cbcf414159b7b4c090dd42930a4272e83e29fa7
umairnadeem123 <nadeem.umair123@gmail.com>
2019-08-03 20:56:07 -0700

    Initialize Jest/Enzyme testing suite

---
+++ spec/LoginPage.test.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { shallow, render, mount } from 'enzyme';
+import LoginPage from '../client/src/components/LoginPage';
+
+describe('Some behavior', () => {
+  it('should do something', () => {
+  });
+});
