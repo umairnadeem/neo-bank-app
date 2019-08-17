b78e25d0fb29f1ba6e95c0a8d196a394770e0c29
umairnadeem123 <nadeem.umair123@gmail.com>
2019-08-16 17:03:38 -0700

    Add more reducer cases for dashboardReducer

--- client/src/_reducers/dashboardReducers.js
+++ client/src/_reducers/dashboardReducers.js
@@ -1,11 +1,20 @@
-import { dashboardConstants } from '../_constants';
+import { dashboardConstants, userConstants } from '../_constants';
 
 const initialState = {
   trans: null,
+  data: null,
 };
 
 export default function (state = initialState, action) {
   switch (action.type) {
+    case userConstants.LOGIN:
+      return {
+        data: action.payload,
+      };
+    case userConstants.LOGOUT:
+      return {
+        data: null,
+      };
     case dashboardConstants.TRANS:
       return {
         trans: action.payload,
