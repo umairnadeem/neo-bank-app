017fbf1f18c1185eae3aa416bd6cc9f06e800e39
umairnadeem123 <nadeem.umair123@gmail.com>
2019-08-17 17:02:03 -0700

    Make container component Verify to verify credentials for certain comps

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,56 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { userActions } from '../_actions';
 import LoginPage from './Login/LoginPage';
 import Authenticate from './Login/Authenticate';
 import Dashboard from './Dashboard';
-
-const actions = {
-  verify: userActions.verify,
-};
+import Verify from './Containers/Verify';
 
 const mapStateToProps = state => ({
   authentication: state.authentication,
 });
 
-class App extends React.PureComponent {
-  componentDidMount() {
-    const { verify, authentication: { requiresLogin } } = this.props;
-    if (requiresLogin) {
-      verify();
-    }
-  }
-
-  render() {
-    const {
-      authentication:
-      {
-        requiresLogin,
-        requiresAuth,
-        isLoggedIn,
-      },
-    } = this.props;
-
-    return (
-      <div className="flex">
-        {requiresLogin
-          ? <LoginPage />
-          : null }
-        { requiresAuth
-          ? <Authenticate />
-          : null }
-        { isLoggedIn
-          ? <Dashboard />
-          : null }
-      </div>
-    );
-  }
-}
+const App = ({
+  authentication:
+  {
+    requiresLogin,
+    requiresAuth,
+    isLoggedIn,
+  },
+}) => (
+  <Verify>
+    <div className="flex">
+      {requiresLogin
+        ? <LoginPage />
+        : null }
+      { requiresAuth
+        ? <Authenticate />
+        : null }
+      { isLoggedIn
+        ? <Dashboard />
+        : null }
+    </div>
+  </Verify>
+);
 
 App.propTypes = {
   authentication: PropTypes.objectOf(PropTypes.any).isRequired,
-  verify: PropTypes.func.isRequired,
 };
 
-export default connect(mapStateToProps, actions)(App);
+export default connect(mapStateToProps, null)(App);
---
+++ client/src/components/Containers/Verify.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { userActions } from '../../_actions';
+
+const actions = {
+  verify: userActions.verify,
+};
+
+const mapStateToProps = state => ({
+  authentication: state.authentication,
+});
+
+class VerifyContainer extends React.PureComponent {
+  componentDidMount() {
+    const { verify, authentication: { requiresLogin } } = this.props;
+    if (requiresLogin) {
+      verify();
+    }
+  }
+
+  render() {
+    const { children } = this.props;
+    return (
+      <React.Fragment>
+        {children}
+      </React.Fragment>
+    );
+  }
+}
+
+VerifyContainer.propTypes = {
+  authentication: PropTypes.objectOf(PropTypes.any).isRequired,
+  verify: PropTypes.func.isRequired,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]).isRequired,
+};
+
+export default connect(mapStateToProps, actions)(VerifyContainer);
